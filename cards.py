7d80b12b4951bd70388332dddc85d1f46e631d18
mbzos45 <90610406+mbzos45@users.noreply.github.com>
2024-01-25 19:31:25 +0900

    コード追加

---
+++ cards.py
@@ -0,0 +1,98 @@
+import random
+import pygame as pg
+import enum
+from typing import Final
+
+IMG_PATH = "assets/img/"
+CARD_BACK_NAME = "cardBack.png"
+CARD_GAP = (40, 50)
+
+
+class Suit(enum.IntEnum):
+    SPADES = 1
+    CLUBS = 2
+    DIAMONDS = 3
+    HEARTS = 4
+
+
+class Score(enum.IntEnum):
+    A = 1
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
+    SIX = 6
+    SEVEN = 7
+    EIGHT = 8
+    NINE = 9
+    TEN = 10
+    J = 11
+    Q = 12
+    K = 13
+
+    def get_str(self):
+        match self.value:
+            case Score.A:
+                result = "A"
+            case Score.J:
+                result = "J"
+            case Score.Q:
+                result = "Q"
+            case Score.K:
+                result = "K"
+            case _:
+                result = str(self.value)
+        return result
+
+
+class Card:
+    SCORE: Final[Score]
+    SUIT: Final[Suit]
+
+    def __init__(self, suit: Suit, score: Score, back_surface: pg.Surface | pg.SurfaceType,
+                 front_surface: pg.Surface | pg.SurfaceType, x=0, y=0):
+        self.SUIT = suit
+        self.SCORE = score
+        self.back_surface = pg.transform.scale(back_surface, tuple(map(lambda a: a / 2, back_surface.get_size())))
+        self.front_surface = pg.transform.scale(front_surface, tuple(map(lambda a: a / 2, front_surface.get_size())))
+        self.is_opened = False
+        self.surface = self.back_surface
+        self.is_cleared = False
+        self.rect = self.surface.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def open_card(self):
+        self.surface = self.front_surface
+        self.is_opened = True
+
+    def hide_card(self):
+        self.surface = self.back_surface
+        self.is_opened = False
+
+    def clear_card(self):
+        self.is_cleared = True
+
+
+def get_front_img(suit: Suit, score: Score):
+    path = IMG_PATH + "card" + suit.name.capitalize() + score.get_str() + ".png"
+    return pg.image.load(path)
+
+
+def init_cards() -> list[list[Card | None]]:
+    back_img = pg.image.load(IMG_PATH + CARD_BACK_NAME)
+    raw_list = [Card(suit, score, back_img, get_front_img(suit, score)) for suit in Suit for score in Score]
+    random.shuffle(raw_list)
+    suit_len = len(Suit)
+    score_len = len(Score)
+    result: list[list[Card | None]] = [raw_list[i:i + score_len] for i in range(0, suit_len * score_len, score_len)]
+    card_size = result[0][0].surface.get_size()
+    y_pos = 10
+    for single_list in result:
+        x_pos = 10
+        for card in single_list:
+            card.rect.x = x_pos
+            card.rect.y = y_pos
+            x_pos += card_size[0] + CARD_GAP[0]
+        y_pos += card_size[1] + CARD_GAP[1]
+    return result
---
+++ main.py
@@ -0,0 +1,105 @@
+import sys
+from cards import *
+from cards import Card
+
+SCREEN_SIZE = (1410, 550)
+CENTER_POS = tuple(map(lambda a: a / 2, SCREEN_SIZE))
+FRAME_RATE = 24
+BG_COLOR = "green4"
+WINDOW_TITLE = "神経衰弱"
+CARD_BACK_PATH = "assets/img/cardBack.png"
+REPLAY_BTN_PATH = "assets/img/replay.png"
+FLIP_SOUND_PATH = "assets/sound/flip.mp3"
+CORRECT_SOUND_PATH = "assets/sound/correct.mp3"
+CLEAR_SOUND_PATH = "assets/sound/clear.mp3"
+MISS_SOUND_PATH = "assets/sound/miss.mp3"
+BUTTON_SOUND_PATH = "assets/sound/button.mp3"
+
+
+def game_shutdown():
+    pg.quit()
+    sys.exit()
+
+
+def main():
+    pg.init()
+    card_lists: list[list[Card]] = init_cards()
+    card_len = sum(len(i) for i in card_lists)
+    screen = pg.display.set_mode(size=SCREEN_SIZE)
+    pg.display.set_caption(title=WINDOW_TITLE)
+    card_back_surface = pg.image.load(CARD_BACK_PATH)
+    pg.display.set_icon(card_back_surface)
+    opened_cards: list[Card] = []
+    font = pg.font.Font(None, 130)
+    clear_text_surface = font.render("Cleared!", True, pg.Color("RED"))
+    clear_text_rect = clear_text_surface.get_rect(center=CENTER_POS)
+    replay_btn_surface = pg.image.load(REPLAY_BTN_PATH)
+    replay_btn_rect = replay_btn_surface.get_rect(center=(CENTER_POS[0], CENTER_POS[1] + 200))
+    cleared_card = 0
+    clear_sound = pg.mixer.Sound(CLEAR_SOUND_PATH)
+    miss_sound = pg.mixer.Sound(MISS_SOUND_PATH)
+    correct_sound = pg.mixer.Sound(CORRECT_SOUND_PATH)
+    flip_sound = pg.mixer.Sound(FLIP_SOUND_PATH)
+    button_sound = pg.mixer.Sound(BUTTON_SOUND_PATH)
+    is_cleared = False
+    while True:
+        try:
+            screen.fill(pg.Color(BG_COLOR))
+            m_down = pg.mouse.get_pressed()
+            mouse_pos = pg.mouse.get_pos()
+            if is_cleared:
+                screen.blit(clear_text_surface, clear_text_rect)
+                btn = screen.blit(replay_btn_surface, replay_btn_rect)
+                if m_down[0]:
+                    if btn.collidepoint(mouse_pos):
+                        button_sound.play()
+                        is_cleared = False
+                        cleared_card = 0
+                        card_lists = init_cards()
+                pass
+            else:
+                for col in card_lists:
+                    for card in col:
+                        if card.is_cleared:
+                            continue
+                        target_rect = screen.blit(card.surface, card.rect)
+                        if card in opened_cards:
+                            continue
+                        if m_down[0] and target_rect.collidepoint(mouse_pos):
+                            card.open_card()
+                            screen.blit(card.surface, card.rect)
+                            flip_sound.play()
+                            if len(opened_cards) < 2:
+                                opened_cards.append(card)
+            pg.display.update()
+            pg.time.Clock().tick(FRAME_RATE)
+            if len(opened_cards) == 2:
+                pg.time.wait(500)
+                if opened_cards[0].SCORE == opened_cards[1].SCORE:
+                    for card in opened_cards:
+                        card.clear_card()
+                        cleared_card += 1
+                    if cleared_card >= card_len:
+                        is_cleared = True
+                        clear_sound.play()
+                    else:
+                        correct_sound.play()
+                else:
+                    for card in opened_cards:
+                        card.hide_card()
+                    miss_sound.play()
+                opened_cards.clear()
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    game_shutdown()
+                if event.type == pg.KEYDOWN:
+                    if event.key == pg.K_ESCAPE:
+                        game_shutdown()
+        except KeyboardInterrupt:
+            print("shutdown by keyboard input")
+            game_shutdown()
+    pass
+
+
+if __name__ == "__main__":
+    main()
